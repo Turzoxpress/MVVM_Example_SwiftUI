ea3a0d6426a1125ce4e73e2e1f19b30d154b5d74
Turzoxpress <turzoxpress@gmail.com>
2021-05-26 14:31:34 +0600

    Initial Commit

---
+++ MVVM Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM Example
+//
+//  Created by Mahbubbur Rahman Turzo on 26/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM Example/MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_ExampleApp.swift
+//  MVVM Example
+//
+//  Created by Mahbubbur Rahman Turzo on 26/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
